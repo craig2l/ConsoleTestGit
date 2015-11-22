54f80578b612777c74e1020202941194e47a03e4
Craig Lyall <Craig@D-LCorp.com>
2015-11-21 16:49:25 -0800

    Initial project checkin.

---
+++ ConsoleTestGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
